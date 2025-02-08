d6e9fb9af486f3be54d16e4aa3ee99b953f96e39
krishi-agrawal <krishi.agrawal26@gmail.com>
2025-02-08 10:12:25 +0530

    login n registration pages edits

--- client/src/layouts/Admin/Admin.jsx
+++ client/src/layouts/Admin/Admin.jsx
@@ -1,46 +1,38 @@
 import React from "react";
 import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import PerfectScrollbar from "perfect-scrollbar";
-import { ethers } from "ethers"; // Import Ethers.js
 
 // Core Components
 import AdminNavbar from "../../components/Navbars/AdminNavbar";
 import Footer from "../../components/Footer/Footer";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import FixedPlugin from "../../components/FixedPlugin/FixedPlugin";
 import Dashboard from "../../views/Dashboard";
 import routes from "../../routes";
 
 import logo from "../../assets/img/react-logo.png";
 import { BackgroundColorContext } from "../../contexts/BackgroundColorContext";
 
-var ps;
+let ps;
 
-function Admin(props) {
+function Admin() {
   const location = useLocation();
   const mainPanelRef = React.useRef(null);
-  const [sidebarOpened, setsidebarOpened] = React.useState(
+  const [sidebarOpened, setSidebarOpened] = React.useState(
     document.documentElement.className.indexOf("nav-open") !== -1
   );
 
-  // Initialize Ethers.js provider (example)
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-
   React.useEffect(() => {
     if (navigator.platform.indexOf("Win") > -1) {
-      document.documentElement.className += " perfect-scrollbar-on";
+      document.documentElement.classList.add("perfect-scrollbar-on");
       document.documentElement.classList.remove("perfect-scrollbar-off");
       ps = new PerfectScrollbar(mainPanelRef.current, {
         suppressScrollX: true,
       });
       let tables = document.querySelectorAll(".table-responsive");
-      for (let i = 0; i < tables.length; i++) {
-        ps = new PerfectScrollbar(tables[i]);
-      }
+      tables.forEach((table) => new PerfectScrollbar(table));
     }
-    // Cleanup
-    return function cleanup() {
-      if (navigator.platform.indexOf("Win") > -1) {
+    return () => {
+      if (navigator.platform.indexOf("Win") > -1 && ps) {
         ps.destroy();
         document.documentElement.classList.add("perfect-scrollbar-off");
         document.documentElement.classList.remove("perfect-scrollbar-on");
@@ -49,12 +41,6 @@ function Admin(props) {
   }, []);
 
   React.useEffect(() => {
-    if (navigator.platform.indexOf("Win") > -1) {
-      let tables = document.querySelectorAll(".table-responsive");
-      for (let i = 0; i < tables.length; i++) {
-        ps = new PerfectScrollbar(tables[i]);
-      }
-    }
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
     if (mainPanelRef.current) {
@@ -64,22 +50,15 @@ function Admin(props) {
 
   const toggleSidebar = () => {
     document.documentElement.classList.toggle("nav-open");
-    setsidebarOpened(!sidebarOpened);
+    setSidebarOpened(!sidebarOpened);
   };
 
   const getRoutes = (routes) => {
     return routes.map((prop, key) => {
       if (prop.layout === "/admin") {
-        return (
-          <Route
-            path={prop.layout + prop.path}
-            component={prop.component}
-            key={key}
-          />
-        );
-      } else {
-        return null;
+        return <Route path={prop.path} element={<prop.component />} key={key} />;
       }
+      return null;
     });
   };
 
@@ -94,49 +73,41 @@ function Admin(props) {
 
   return (
     <BackgroundColorContext.Consumer>
-      {({ color, changeColor }) => (
-        <React.Fragment>
-          <div className="flex h-screen bg-gray-100">
-            {/* Sidebar */}
-            <Sidebar
-              routes={routes}
-              logo={{
-                outterLink: "#",
-                text: "Land Registration",
-                imgSrc: logo,
-              }}
+      {({ color }) => (
+        <div className="flex h-screen bg-gray-100">
+          {/* Sidebar */}
+          <Sidebar
+            routes={routes}
+            logo={{ outterLink: "#", text: "Land Registration", imgSrc: logo }}
+            toggleSidebar={toggleSidebar}
+          />
+
+          {/* Main Panel */}
+          <div
+            className="flex-1 flex flex-col overflow-hidden"
+            ref={mainPanelRef}
+            data-color={color}
+          >
+            {/* Navbar */}
+            <AdminNavbar
+              brandText={getBrandText(location.pathname)}
               toggleSidebar={toggleSidebar}
+              sidebarOpened={sidebarOpened}
             />
 
-            {/* Main Panel */}
-            <div
-              className="flex-1 flex flex-col overflow-hidden"
-              ref={mainPanelRef}
-              data={color}
-            >
-              {/* Navbar */}
-              <AdminNavbar
-                brandText={getBrandText(location.pathname)}
-                toggleSidebar={toggleSidebar}
-                sidebarOpened={sidebarOpened}
-              />
+            {/* Main Content */}
+            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 p-6">
+              <Routes>
+                {getRoutes(routes)}
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
+              </Routes>
+            </main>
 
-              {/* Main Content */}
-              <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 p-6">
-                <Routes>
-                  {getRoutes(routes)}
-                  <Navigate to="/admin/dashboard" replace />
-                </Routes>
-              </main>
-
-              {/* Footer */}
-              <Footer fluid />
-            </div>
+            {/* Footer */}
+            <Footer fluid />
           </div>
-
-          {/* Fixed Plugin (Optional) */}
-          {/* <FixedPlugin bgColor={color} handleBgClick={changeColor} /> */}
-        </React.Fragment>
+        </div>
       )}
     </BackgroundColorContext.Consumer>
   );
